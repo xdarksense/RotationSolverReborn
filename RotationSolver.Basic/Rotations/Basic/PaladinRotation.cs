00f6caf6ff695844b72ba552412cd4c5d836c348
LTS-FFXIV <127939494+LTS-FFXIV@users.noreply.github.com>
2024-07-25 09:56:51 -0500

    PLD Base2.0 update, should resolve issues with using mits

--- RotationSolver.Basic/Rotations/Basic/PaladinRotation.cs
+++ RotationSolver.Basic/Rotations/Basic/PaladinRotation.cs
@@ -27,10 +27,189 @@ partial class PaladinRotation
 
     #region Job Gauge
     /// <summary>
-    /// 
+    /// Gets the current level of the Oath gauge.
     /// </summary>
     public static byte OathGauge => JobGauge.OathGauge;
     #endregion
+
+    private protected sealed override IBaseAction TankStance => IronWillPvE;
+
+    static partial void ModifyFastBladePvE(ref ActionSetting setting)
+    {
+
+    }
+
+    static partial void ModifyFightOrFlightPvE(ref ActionSetting setting)
+    {
+        setting.StatusProvide = [StatusID.GoringBladeReady];
+        setting.CreateConfig = () => new()
+        {
+            TimeToKill = 0,
+        };
+    }
+
+    static partial void ModifyRiotBladePvE(ref ActionSetting setting)
+    {
+        setting.ComboIds = [ActionID.FastBladePvE];
+    }
+
+    static partial void ModifyTotalEclipsePvE(ref ActionSetting setting)
+    {
+        setting.CreateConfig = () => new()
+        {
+            AoeCount = 2,
+        };
+    }
+
+    static partial void ModifyShieldBashPvE(ref ActionSetting setting)
+    {
+        setting.ActionCheck = () => ActionID.LowBlowPvE.IsCoolingDown();
+        setting.TargetStatusProvide = [StatusID.Stun];
+        setting.TargetType = TargetType.Interrupt;
+        //setting.CanTarget = o =>
+        //{
+        //    if (o is not IBattleChara b) return false;
+
+        //    if (b.IsBossFromIcon() || IsMoving || b.CastActionId == 0) return false;
+
+        //    if (!b.IsCastInterruptible || ActionID.InterjectPvE.IsCoolingDown()) return true;
+        //    return false;
+        //};
+    }
+
+    static partial void ModifyShieldLobPvE(ref ActionSetting setting)
+    {
+        setting.SpecialType = SpecialActionType.MeleeRange;
+        setting.UnlockedByQuestID = 65798;
+    }
+
+    static partial void ModifyRageOfHalonePvE(ref ActionSetting setting)
+    {
+        setting.ComboIds = [ActionID.RiotBladePvE];
+    }
+
+    static partial void ModifySpiritsWithinPvE(ref ActionSetting setting)
+    {
+        setting.UnlockedByQuestID = 66591;
+    }
+
+    static partial void ModifySheltronPvE(ref ActionSetting setting)
+    {
+        setting.ActionCheck = () => OathGauge >= 50;
+        setting.StatusProvide = [StatusID.Sheltron];
+        setting.UnlockedByQuestID = 66592;
+        setting.TargetType = TargetType.Self;
+    }
+
+    static partial void ModifySentinelPvE(ref ActionSetting setting)
+    {
+        setting.StatusProvide = StatusHelper.RampartStatus;
+        setting.TargetType = TargetType.Self;
+    }
+
+    static partial void ModifyProminencePvE(ref ActionSetting setting)
+    {
+        setting.StatusProvide = [StatusID.DivineMight];
+        setting.ComboIds = [ActionID.TotalEclipsePvE];
+        setting.UnlockedByQuestID = 66593;
+        setting.CreateConfig = () => new()
+        {
+            AoeCount = 2,
+        };
+    }
+
+    static partial void ModifyCoverPvE(ref ActionSetting setting)
+    {
+        setting.StatusProvide = [StatusID.Cover];
+        setting.TargetStatusProvide = [StatusID.Covered];
+        setting.ActionCheck = () => OathGauge >= 50;
+        setting.UnlockedByQuestID = 66595;
+        setting.TargetType = TargetType.BeAttacked;
+    }
+
+    static partial void ModifyCircleOfScornPvE(ref ActionSetting setting)
+    {
+        setting.TargetStatusProvide = [StatusID.CircleOfScorn];
+        setting.CreateConfig = () => new()
+        {
+            AoeCount = 2,
+        };
+    }
+
+    static partial void ModifyHallowedGroundPvE(ref ActionSetting setting)
+    {
+        setting.StatusProvide = [StatusID.HallowedGround];
+        setting.UnlockedByQuestID = 66596;
+    }
+
+    static partial void ModifyBulwarkPvE(ref ActionSetting setting)
+    {
+        setting.StatusProvide = [StatusID.Bulwark];
+        setting.UnlockedByQuestID = 66596;
+    }
+
+    static partial void ModifyGoringBladePvE(ref ActionSetting setting)
+    {
+        setting.UnlockedByQuestID = 67570;
+        setting.StatusNeed = [StatusID.GoringBladeReady];
+    }
+
+    static partial void ModifyDivineVeilPvE(ref ActionSetting setting)
+    {
+        setting.UnlockedByQuestID = 67571;
+        setting.StatusProvide = [StatusID.DivineVeil_1362];
+    }
+
+    static partial void ModifyClemencyPvE(ref ActionSetting setting)
+    {
+        setting.UnlockedByQuestID = 67572;
+    }
+
+    static partial void ModifyRoyalAuthorityPvE(ref ActionSetting setting)
+    {
+        setting.StatusProvide = [StatusID.DivineMight, StatusID.AtonementReady];
+        setting.ComboIds = [ActionID.RiotBladePvE];
+        setting.UnlockedByQuestID = 67573;
+    }
+
+    static partial void ModifyInterventionPvE(ref ActionSetting setting)
+    {
+        setting.ActionCheck = () => OathGauge >= 50;
+        setting.TargetStatusProvide = [StatusID.KnightsResolve, StatusID.KnightsBenediction, StatusID.Intervention];
+    }
+
+    static partial void ModifyHolySpiritPvE(ref ActionSetting setting)
+    {
+        
+    }
+
+    static partial void ModifyRequiescatPvE(ref ActionSetting setting)
+    {
+        setting.StatusProvide = [StatusID.ConfiteorReady];
+        setting.CreateConfig = () => new()
+        {
+            TimeToKill = 0,
+        };
+    }
+
+    static partial void ModifyPassageOfArmsPvE(ref ActionSetting setting)
+    {
+        setting.StatusProvide = [StatusID.PassageOfArms];
+        setting.UnlockedByQuestID = 68111;
+    }
+
+    static partial void ModifyHolyCirclePvE(ref ActionSetting setting)
+    {
+        setting.CreateConfig = () => new()
+        {
+            AoeCount = 2,
+        };
+    }
+
+    static partial void ModifyIntervenePvE(ref ActionSetting setting)
+    {
+        setting.SpecialType = SpecialActionType.MovingForward;
+    }
 
     static partial void ModifyAtonementPvE(ref ActionSetting setting)
     {
@@ -49,134 +228,81 @@ partial class PaladinRotation
         setting.StatusNeed = [StatusID.SepulchreReady];
     }
 
-    static partial void ModifyShieldBashPvE(ref ActionSetting setting)
-    {
-        setting.CanTarget = o =>
-        {
-            if (o is not IBattleChara b) return false;
-
-            if (b.IsBossFromIcon() || IsMoving || b.CastActionId == 0) return false;
-
-            if (!b.IsCastInterruptible || ActionID.InterjectPvE.IsCoolingDown()) return true;
-            return false;
-        };
-        setting.ActionCheck = () => ActionID.LowBlowPvE.IsCoolingDown();
-        setting.StatusProvide = [StatusID.Stun];
-    }
-
-    static partial void ModifyShieldLobPvE(ref ActionSetting setting)
-    {
-        setting.SpecialType = SpecialActionType.MeleeRange;
-        setting.UnlockedByQuestID = 65798;
-    }
-
-    static partial void ModifySheltronPvE(ref ActionSetting setting)
-    {
-        setting.UnlockedByQuestID = 66592;
-    }
-
-    private protected sealed override IBaseAction TankStance => IronWillPvE;
-
-    static partial void ModifyRequiescatPvE(ref ActionSetting setting)
-    {
-        setting.CreateConfig = () => new()
-        {
-            TimeToKill = 0,
-        };
-    }
-
     static partial void ModifyConfiteorPvE(ref ActionSetting setting)
     {
         setting.StatusNeed = [StatusID.ConfiteorReady];
-    }
-
-    static partial void ModifyBladeOfHonorPvE(ref ActionSetting setting)
-    {
-        setting.StatusNeed = [StatusID.BladeOfHonorReady];
-    }
-
-
-    static partial void ModifyFightOrFlightPvE(ref ActionSetting setting)
-    {
         setting.CreateConfig = () => new()
         {
-            TimeToKill = 0,
+            AoeCount = 1,
         };
-        setting.StatusProvide = [StatusID.GoringBladeReady];
-    }
-
-    static partial void ModifySentinelPvE(ref ActionSetting setting)
-    {
-        setting.StatusProvide = StatusHelper.RampartStatus;
-        setting.ActionCheck = Player.IsTargetOnSelf;
-    }
-
-    static partial void ModifyBulwarkPvE(ref ActionSetting setting)
-    {
-        setting.StatusProvide = StatusHelper.RampartStatus;
-        setting.ActionCheck = Player.IsTargetOnSelf;
-    }
-
-    static partial void ModifyCoverPvE(ref ActionSetting setting)
-    {
-        setting.ActionCheck = () => OathGauge >= 50;
-        setting.UnlockedByQuestID = 66595;
-    }
-
-    static partial void ModifyIntervenePvE(ref ActionSetting setting)
-    {
-        setting.ActionCheck = () => OathGauge >= 50;
     }
 
     static partial void ModifyHolySheltronPvE(ref ActionSetting setting)
     {
-        setting.ActionCheck = () => OathGauge >= 50 && Player.IsTargetOnSelf();
+        setting.ActionCheck = () => OathGauge >= 50;
+        setting.StatusProvide = [StatusID.HolySheltron];
+        setting.UnlockedByQuestID = 66592;
+        setting.TargetType = TargetType.Self;
     }
 
-    static partial void ModifySpiritsWithinPvE(ref ActionSetting setting)
+    static partial void ModifyExpiacionPvE(ref ActionSetting setting)
     {
-        setting.UnlockedByQuestID = 66591;
+        setting.CreateConfig = () => new()
+        {
+            AoeCount = 1,
+        };
     }
 
-    static partial void ModifyProminencePvE(ref ActionSetting setting)
+    static partial void ModifyBladeOfFaithPvE(ref ActionSetting setting)
     {
-        setting.UnlockedByQuestID = 66593;
-        setting.StatusProvide = [StatusID.DivineMight];
+        setting.ComboIds = [ActionID.ConfiteorPvE];
+        setting.CreateConfig = () => new()
+        {
+            AoeCount = 1,
+        };
     }
 
-    static partial void ModifyHallowedGroundPvE(ref ActionSetting setting)
+    static partial void ModifyBladeOfTruthPvE(ref ActionSetting setting)
     {
-        setting.UnlockedByQuestID = 66596;
+        setting.ComboIds = [ActionID.BladeOfFaithPvE];
+        setting.CreateConfig = () => new()
+        {
+            AoeCount = 1,
+        };
     }
 
-    static partial void ModifyGoringBladePvE(ref ActionSetting setting)
+    static partial void ModifyBladeOfValorPvE(ref ActionSetting setting)
     {
-        setting.UnlockedByQuestID = 67570;
-        setting.StatusNeed = [StatusID.GoringBladeReady];
+        setting.ComboIds = [ActionID.BladeOfTruthPvE];
+        setting.CreateConfig = () => new()
+        {
+            AoeCount = 1,
+        };
     }
 
-    static partial void ModifyDivineVeilPvE(ref ActionSetting setting)
+    static partial void ModifyGuardianPvE(ref ActionSetting setting)
     {
-        setting.UnlockedByQuestID = 67571;
+        setting.StatusProvide = StatusHelper.RampartStatus;
     }
 
-    static partial void ModifyClemencyPvE(ref ActionSetting setting)
+    static partial void ModifyImperatorPvE(ref ActionSetting setting)
     {
-        setting.UnlockedByQuestID = 67572;
+        setting.StatusProvide = [StatusID.ConfiteorReady, StatusID.Requiescat];
+        setting.CreateConfig = () => new()
+        {
+            AoeCount = 1,
+        };
     }
-
-    static partial void ModifyRoyalAuthorityPvE(ref ActionSetting setting)
+    static partial void ModifyBladeOfHonorPvE(ref ActionSetting setting)
     {
-        setting.UnlockedByQuestID = 67573;
-        setting.StatusProvide = [StatusID.DivineMight];
-        setting.StatusProvide = [StatusID.AtonementReady];
+        setting.StatusNeed = [StatusID.BladeOfHonorReady];
+        setting.CreateConfig = () => new()
+        {
+            AoeCount = 1,
+        };
     }
 
-    static partial void ModifyPassageOfArmsPvE(ref ActionSetting setting)
-    {
-        setting.UnlockedByQuestID = 68111;
-    }
-
+    // PvP
     static partial void ModifyIntervenePvP(ref ActionSetting setting)
     {
         setting.SpecialType = SpecialActionType.MovingForward;
