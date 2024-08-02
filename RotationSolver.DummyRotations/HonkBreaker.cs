82e4fcbbe9388fd9853449a2f88700c3fee2b097
LTS <127939494+LTS-FFXIV@users.noreply.github.com>
2024-08-01 19:16:06 -0500

    Update HonkBreaker.cs

--- RotationSolver.DummyRotations/HonkBreaker.cs
+++ RotationSolver.DummyRotations/HonkBreaker.cs
@@ -8,7 +8,7 @@ namespace RotationSolver.DummyRotations;
 [Rotation("TestingRotation", CombatType.PvE, GameVersion = "7.05")]
 [Api(3)]
 
-public sealed class TestingRotation : AstrologianRotation
+public sealed class TestingRotation : PictomancerRotation
 {
     #region Additional oGCD Logic
 
@@ -22,7 +22,8 @@ public sealed class TestingRotation : AstrologianRotation
     [RotationDesc]
     protected override bool MoveForwardAbility(IAction nextGCD, out IAction? act)
     {
-        return base.AttackAbility(nextGCD, out act);
+        
+        return base.MoveForwardAbility(nextGCD, out act);
     }
 
     [RotationDesc]
@@ -61,41 +62,57 @@ public sealed class TestingRotation : AstrologianRotation
     #endregion
 
     #region oGCD Logic
+
+    protected override bool GeneralAbility(IAction nextGCD, out IAction? act)
+    {
+
+        return base.GeneralAbility(nextGCD, out act);
+    }
+
     protected override bool AttackAbility(IAction nextGCD, out IAction? act)
     {
+        
         return base.AttackAbility(nextGCD, out act);
     }
     #endregion
 
     #region GCD Logic
-    protected override bool GeneralGCD(out IAction? act)
-    {
-        return base.GeneralGCD(out act);
-    }
 
     protected override bool MoveForwardGCD(out IAction? act)
     {
+       
         return base.MoveForwardGCD(out act);
-    }
-
-    protected override bool HealAreaGCD(out IAction? act)
-    {
-        return base.HealAreaGCD(out act);
     }
 
     protected override bool DefenseAreaGCD(out IAction? act)
     {
+        
         return base.DefenseAreaGCD(out act);
     }
 
     protected override bool DefenseSingleGCD(out IAction? act)
     {
+        
         return base.DefenseSingleGCD(out act);
+    }
+
+    protected override bool HealAreaGCD(out IAction? act)
+    {
+        
+        return base.HealAreaGCD(out act);
     }
 
     protected override bool HealSingleGCD(out IAction? act)
     {
+        
         return base.HealSingleGCD(out act);
     }
+
+    protected override bool GeneralGCD(out IAction? act)
+    {
+        
+        return base.GeneralGCD(out act);
+    }
+
     #endregion
 }
